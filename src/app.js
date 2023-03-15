1454d4b313a69a22723ea66614b20e195c45d30c
zvidmarb <50197931+zvidmarb@users.noreply.github.com>
2023-03-14 17:57:10 -0700

    satisfied with landing layout for now, dropdown functionality added

---
+++ src/app.js
@@ -0,0 +1,4 @@
+function dropdown() {
+    var drop = document.getElementsByClassName("dropdown")[0];
+    (drop.style.display == "none") ?  drop.style.display = "flex" : drop.style.display = "none";
+}
