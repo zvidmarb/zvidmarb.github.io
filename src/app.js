ab49a86ee02b736eb28263c069b3abc9978591c6
zvidmarb <50197931+zvidmarb@users.noreply.github.com>
2023-11-07 23:56:57 -0800

    made initial media query based on intuition, fix for mobile drop

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 function dropdown() {
-    var drop = document.getElementById("dropdown");
+    var drop = document.getElementById("nav-items");
     (drop.style.display == "none") ?  drop.style.display = "flex" : drop.style.display = "none";
 }
