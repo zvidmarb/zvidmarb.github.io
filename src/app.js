c55c99cdfa90598d389e3934b5439be893e46d75
zvidmarb <zvidmarb@ucsd.edu>
2023-12-14 11:53:57 -0800

    wanted to implement the 8pt system automatically, css round() too fresh, manual JS function not working

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+document.querySelectorAll('.number').forEach(function(number){
+    /* ROUND BY 4 ON ALL NUMBERS FOR 8PT SYSTEM */
+    let val = Math.trunc(parseFloat(number.innerHTML));
+    let diff = val % 4;
+    number.innerHTML = (diff < 2) ? val - diff : val + (4 - diff); //0 1 / 2 3 0
+});
+
 function dropdown() {
     var drop = document.getElementById("nav-items");
     (drop.style.display == "none") ?  drop.style.display = "flex" : drop.style.display = "none";
