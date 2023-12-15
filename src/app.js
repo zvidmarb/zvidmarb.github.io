785dcb62eccb1605f68e449c18b7c7d7fb100081
zvidmarb <zvidmarb@ucsd.edu>
2023-12-14 17:18:31 -0800

    outdated fonts, responsive blurb section, various L-XL fixes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-document.querySelectorAll('.number').forEach(function(number){
+/* TODO doesn't seem to be running? */
+document.querySelectorAll('.number').forEach(function(number){ 
     /* ROUND BY 4 ON ALL NUMBERS FOR 8PT SYSTEM */
     let val = Math.trunc(parseFloat(number.innerHTML));
+    console.log(val);
     let diff = val % 4;
     number.innerHTML = (diff < 2) ? val - diff : val + (4 - diff); //0 1 / 2 3 0
 });
