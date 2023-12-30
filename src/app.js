b9cf789c4279a593fe00b1bdabfc82e92ec738c0
zvidmarb <50197931+zvidmarb@users.noreply.github.com>
2023-12-30 00:40:42 -0800

    functionality planning

--- src/app.js
+++ src/app.js
@@ -6,8 +6,14 @@ document.querySelectorAll('.number').forEach(function(number){
     let diff = val % 4;
     number.innerHTML = (diff < 2) ? val - diff : val + (4 - diff); //0 1 / 2 3 0
 });
-
+/*event < menu < content > nav > nav-items : swap display between flex and none */
 function dropdown() {
     var drop = document.getElementById("nav-items");
+    (drop.style.display == "none") ?  drop.style.display = "flex" : drop.style.display = "none";
+}
+/*event < chevron < card-header < project-card #id > card-expand : swap display between flex and none
+    NOTE also change style of chevron */
+function expandCard(elem) {
+    var elem = document.getElementById("")
     (drop.style.display == "none") ?  drop.style.display = "flex" : drop.style.display = "none";
 }
