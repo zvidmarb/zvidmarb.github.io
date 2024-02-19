874e26b4616da96b136883e50f62420652a5a68c
zvidmarb <50197931+zvidmarb@users.noreply.github.com>
2024-02-18 23:29:12 -0800

    added observer class design for updating elements modularly based on views

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -32,110 +32,166 @@
    project-section: responsiveLayout()
    project-card: responsiveLayout(), responsive(), icon-media-card, hoverEvent
  */
+
+
+/* TODO doesn't seem to be running?
+document.querySelectorAll('.number').forEach(function(number){ 
+    /* ROUND BY 4 ON ALL NUMBERS FOR 8PT SYSTEM
+    let val = Math.trunc(parseFloat(number.innerHTML));
+    console.log(val);
+    let diff = val % 4;
+    number.innerHTML = (diff < 2) ? val - diff : val + (4 - diff); //0 1 / 2 3 0
+});
+*/
 /* CONSTANTS, HELPERS */
 const MOBILE = 0, DESKTOP = 1;
 const S = 600, M = 905, L = 1240, XL = 1440; //minimum breakpoints of each screen size, XS-M: mobile/tablet, L-XL: desktop 
-function ResponsiveCard(node) {
-    this.node = node;
-    this.toggle = node.querySelector("[data-js-toggle]");
-    this.href = toggle.dataset.href;
-    this.details = node.querySelector("[data-js-expand]");
-}
 
+class ResponsiveCard extends ResponsiveElem {
+    constructor (elem) {
+        super(elem);
+        this.toggle = this.elem.querySelector("[data-js-toggle]");
+        this.link = this.toggle.getAttribute("data-href");
+        this.details = this.elem.querySelector("[data-js-expand]");
+        this.update();
+    }
+    expanded = false;
+    
+    changeIcon() {
+        this.toggle.innerHTML = (!expanded && State.isViewMobile) ? "expand_less" : 
+        (State.isViewMobile) ? "expand_more" : "chevron_right"; 
+    }
+    changeLink() {
+        this.toggle.href = (this.toggle.href == "#") ? this.link : "#";
+    }
+    update() {
+        super.update();
+        this.changeIcon();
+        this.changeLink();
+        updateCardEvents(this.elem, this.toggle);
+    }
+
+}
+class ResponsiveNav extends ResponsiveElem {
+    constructor (element) {
+        super(element);
+        this.dropdown = this.elem.querySelector("[data-js-dropdown]");
+        this.toggle = this.elem.querySelector("[data-js-toggle]");
+    }
+
+    update() {
+        super.update();
+        switchNav();
+    }
+    switchNav() {
+        collapseElement(this.dropdown);
+        this.dropdown.classList.toggle("-fixed", State.isViewDesktop);
+    }
+}
+class ResponsiveSection extends ResponsiveElem {
+    constructor (element) {
+        super(element);
+        this.children = this.node.querySelectorAll("[data-js-responsive]").forEach(
+        function (child) {
+            if ('js-expandable-card' in child.dataset) { //card element case
+                return new ResponsiveCard(child);
+            } else if ('js-collapsible-nav' in child.dataset) {
+                return new ResponsiveNav(child);
+            }
+            return new ResponsiveElem(child); //normal element case
+        })
+    }
+    update() {
+        super.update();
+        this.children.forEach(c => c.update());
+    }
+
+}
+class ResponsiveElem {
+    constructor (element) {
+        this.elem = element;
+    }
+    update() {
+        State.isViewDesktop && this.elem.classList.replace("-mobile", "-desktop");
+        State.isViewMobile && this.elem.classList.replace("-desktop", "-mobile");
+        console.log("updated classes: " + this.elem.class);
+    }
+}
 /* STATE */
 let State = function () {
     this.view = (window.innerWidth < L) ? MOBILE : DESKTOP;
+    this.Responsives = root.querySelectorAll('> [data-js-responsive]')
+                                .forEach(s => new ResponsiveSection(s)); 
 }
 
 /* STATE ACCESSOR FUNCTIONS */
 State.getView = () => this.view;
-State.setViewMobile = () => state.view = MOBILE;
-State.setViewDesktop = () => state.view = DESKTOP;
-State.isViewMobile = () => state.view === MOBILE;
-State.isViewDesktop = () => state.view === DESKTOP;
+State.setViewMobile = () => this.view = MOBILE;
+State.setViewDesktop = () => this.view = DESKTOP;
+State.isViewMobile = () => this.view === MOBILE;
+State.isViewDesktop = () => this.view === DESKTOP;
+
 
 /* DOM node references */
-
+const root = document.querySelector('[data-js-responsive-layout]');
 
 let nav = document.querySelector('[data-js-collapsible-nav]');
-let dropdown = nav.querySelector('[data-js-dropdown]');
+let navDropdown = nav.querySelector('[data-js-dropdown]');
 let toggle = nav.querySelector('[data-js-toggle]');
+
 /* project-card, expandable-card.js:
     toggleElement() - toggles expanded class for card details and for card icon
     changeIcon() - updates icon inner text to change to selected material name
     changeLink() - updates href link "#" <-> link from button href
     project-card, hover (desktop): toggleElement on details
-    project-card, click (mobile): toggleELement on details/card icon, changeIcon more<->less
+    project-card, click (mobile): toggleElement on details/card icon, changeIcon more<->less
 */
 
-let cards = document.querySelectorAll("[data-js-expandable-card]").forEach(el => new ResponsiveCard(el));
-
 /* DOM update functions */
-var toggleElement = element => element.classList.toggle("-expanded");
-var collapseElement = element => element.classList.toggle("-expanded", false);
-function switchLayout (element) {
-    if (isViewMobile) {
-        element.classList.replace("-mobile", "-desktop");
-    } else {
-        element.classList.replace("-desktop", "-mobile");
-    }
-    console.log("switchLayout results in: " + element.class);
-}
-function changeIcon(element, isMobile) {
-    element.innerHTML = (element.innerHTML == "expand_more" && isMobile) ? "expand_less" : 
-                            (isMobile) ? "expand_more" : "chevron_right"; 
-}
-function changeLink(element) {
-    anchor.href = (anchor.href == '#') ? element.getAttribute("data-href") : "#";
-}
+const toggleElement = element => element.classList.toggle("-expanded");
+const collapseElement = element => element.classList.toggle("-expanded", false);
 
+function updateViewUI() {
+    
+    State.Responsives.forEach(update)
+}
 
 /* site-nav, collapsible-nav.js:
     toggleDropdown() - toggles expanded class
     switchNav() - toggle fixed class on nav-items, toggleDropdown if expanded class is present
     icon-media-nav, clickEvent: toggleDropdown on nav-items
 */
-function switchNav(nav) {
-    App.collapseElement(nav);
-    return nav.classList.toggle("-fixed");
-}
+
 
 /* Event Handlers */
-function topLevelResponsive() {
-    var start = document.querySelector("[data-js-responsive]");
-    //top-level?
-    start.querySelectorAll("[data-js-responsive]").forEach(elem => switchLayout(elem));
-    
-    //header - switchLayout() on site-nav; switchNav() on navitems
-
-    //hero - switchLayout() on hero-section
-
-    //projects - switchLayout() on project-section, project-cards; 
-    //           changeicon()s, changelink()s, change event listeners
-
-    //footer?
-}
-CollapsibleNav.responsive = switchNav(dropdown);
-//project-section responsive
-function responsive(element) {
-
-    changeLink(element);
-    changeIcon(element, isMobile);
-    // mobile version - changeicon, changelink, add click, remove hover events
-
-    // desktop version - changeicon, changelink, add hover events, remove click
-
+function onResize() {
+    (window.innerWidth < L) && State.setViewMobile;
+    (window.innerWidth >= L) && State.setViewDesktop;
+    updateViewUI();
 }
 
-function cardExpand(card) {
+function onCardClick() {
     toggleElement(card.details);
-    changeIcon(card.toggle, isMobile);
+    changeIcon(card.toggle, State.isViewMobile);
+}
+function onCardHover() {
+    toggleElement(card.details);
+    console.log("hover event triggered");
 }
 
 /* Event Handler Bindings */
-window.addEventListener("resize", topLevelResponsive());
+window.addEventListener("resize", onResize);
 toggle.addEventListener('click', toggleElement(dropdown));
-var projectCard = document.querySelectorAll('[data-js-expandable-card]').forEach(addEventListener('click'))
+
+function updateCardEvents (card, toggle) {
+    if (State.isViewMobile) {
+        toggle.addEventListener("click", onCardClick);
+        ["mouseenter", "mouseleave"].forEach(t => card.removeEventListener(t, onCardHover));
+    } else if (State.isViewDesktop) {
+        ["mouseenter", "mouseleave"].forEach(t => card.addEventListener(t, onCardHover));
+        toggle.removeEventListener("click", onCardClick);
+    }
+}
 
 /* Initial Setup */
-let state = new State();
+const state = new State();
